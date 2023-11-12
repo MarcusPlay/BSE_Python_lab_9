265b95e8dacd049361f1cf31b87928214539ed9b
marcus <marcusplay@mail.ru>
2023-11-12 16:16:27 +0300

    modified task_2.py

--- task_2.py
+++ task_2.py
@@ -5,13 +5,14 @@
 # c помощью полученного объекта dict_items создайте новый словарь, "обратный" исходному,
 # т. е. ключами являются строки, а значениями – числа.
 
-numbers_v1 = {1:"one", 2:"two", 3:"three"}
-numbers_v2 = {}
+if __name__=="__main__"
+    numbers_v1 = {1:"one", 2:"two", 3:"three"}
+    numbers_v2 = {}
 
-for a, b in numbers_v1.items():
-    numbers_v2[b] = a
+    for a, b in numbers_v1.items():
+        numbers_v2[b] = a
 
-print(numbers_v1)
-print(numbers_v2)
+    print(numbers_v1)
+    print(numbers_v2)
 
 
