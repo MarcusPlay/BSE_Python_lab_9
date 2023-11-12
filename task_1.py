d89d1e161823609bd9c58990007dbe35f49e2739
marcus <marcusplay@mail.ru>
2023-11-12 15:58:18 +0300

    added task_1.py

---
+++ task_1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Cоздайте словарь, связав его с переменной school, и наполните данными, 
+# которые бы отражали количество учащихся в разных классах (1а, 1б, 2б, 6а, 7в и т. п.). 
+# Внесите изменения в словарь согласно следующему: 
+#     а) в одном из классов изменилось количество учащихся, 
+#     б) в школе появился новый класс, 
+#     с) в школе был расформирован (удален) другой класс. 
+#     Вычислите общее количество учащихся в школе.
+
+if __name__=="__main__":
+    classes = {}
+
+    while True:
+        command = input("$ ").lower()
+
+        if command == 'exit':
+            break
+
+        elif command == 'add' or command == "modified":
+            name = input("Введите название класса: ").upper()
+            count_stud = int(input("Введите колличество учащихся в классе: "))
+            classes[name] = count_stud
+
+        elif command == "del":
+            name = input("Введите класс, который хотите удалить: ").upper()
+            del classes[name]
+
+        elif command == "list":
+            line = '+-{}-+-{}-+-{}-+'.format('-' * 4, '-' * 10, '-' * 12)
+            print(line)
+            print('| {:^4} | {:^10} | {:^12} |'.format("№", "Название", "Количество"))
+            print(line)
+
+            a = 1
+            for name, count_stud in classes.items():
+                print('| {:^4} | {:^10} | {:^12} |'.format(a, name, count_stud))
+                a += 1
+            print(line)
