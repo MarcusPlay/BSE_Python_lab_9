156b142d2b2f1c651c9b101f27aa0ab570d3e8f7
Magomedov <69319198+MarcusPlay@users.noreply.github.com>
2023-12-19 18:35:09 +0300

    Update individual_task.py

--- individual_task.py
+++ individual_task.py
@@ -33,8 +33,40 @@ if __name__=="__main__":
 
                 if len(users) > 1:
                     users.sort(key=lambda item: item.get('name', ''))
-
+            
             case 'list':
+                # print('Введите число месяца (1 - 12): ')
+                # while True:
+                #     num = int(input())
+                #     if num < 1 or num > 12:
+                #         print("Значение введено неправильно! Попробуйте еще раз.")
+                #     else:
+                #         break
+                
+                line = '+-{}-+-{}-+-{}-+-{}-+'.format('-' * 4, '-' * 20, '-' * 18, '-' * 10)
+                print(line)
+                print('| {:^4} | {:^20} | {:^18} | {:^10} |'.format(
+                    "№", 
+                    "Название", 
+                    "Номер телефона", 
+                    "Дата"
+                        )
+                    )
+                print(line)
+            
+                for idx, user in enumerate(users, 1):
+                    # if user['year'][1] == num:
+                    print(
+                        '| {:^4} | {:^20} | {:^18} | {:^10} |'.format(
+                            idx, 
+                            user['name'], 
+                            user['phone_number'], 
+                            ' '.join(map(str, user['year']))
+                                )
+                            )
+                    print(line)                
+
+            case 'select':
                 print('Введите число месяца (1 - 12): ')
                 while True:
                     num = int(input())
@@ -62,6 +94,17 @@ if __name__=="__main__":
                                 user['name'], 
                                 user['phone_number'], 
                                 ' '.join(map(str, user['year']))
-                                    )
+                                   )
                                 )
-                        print(line)                
+                        print(line)
+
+            case 'help':
+                print("Список команд:\n")
+                print("add - добавить работника;")
+                print("list - вывести список работников;")
+                print("select <номер месяца> - запросить работников со стажем;")
+                print("help - отобразить справку;")
+                print("exit - завершить работу с программой.")
+
+            case _:
+                print(f"Неизвестная команда {command}")
